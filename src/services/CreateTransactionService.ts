c09d2abb633c52c0256c832df66514e18a466a6e
Nicolas Ferreira <nicolas.ferreiraxn10@gmail.com>
2021-01-17 18:16:09 -0300

    Desafio concluido

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,64 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import TranscationsRepository from '../repositories/TransactionsRepository';
+
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import AppError from '../errors/AppError';
+
+interface RequestDTO {
+  title: string;
+
+  type: 'income' | 'outcome';
+
+  value: number;
+
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    value,
+    category,
+  }: RequestDTO): Promise<Transaction> {
+    const transcationsRepository = getCustomRepository(TranscationsRepository);
+    const categoryRepository = getRepository(Category);
+
+    const { total } = await transcationsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('Você não tem saldo');
+    }
+
+    // Verificar se a categoria já existe
+    let transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(transactionCategory);
+    }
+
+    const transaction = transcationsRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
+
+    await transcationsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,24 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface RequestDTO {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: RequestDTO): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transação não encontrada');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
